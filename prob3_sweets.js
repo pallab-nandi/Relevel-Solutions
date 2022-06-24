608dafe6cb4054598de08945519f0dc562291733
PALLAB NANDI <63771731+pallab-nandi@users.noreply.github.com>
2022-06-25 01:38:47 +0530

    Problem No. 3
    Problem No. 3

---
+++ prob3_sweets.js
@@ -0,0 +1,12 @@
+//To find the sweet distribution is possible or not
+
+function sweet(n) {
+    if(n % 2 === 0) {
+        console.log("yes");
+    } else {
+        console.log("no");
+    }
+}
+
+sweet(8);
+sweet(7);
