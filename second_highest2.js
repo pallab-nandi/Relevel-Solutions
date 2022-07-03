6575f4f3f236840878f9d3a875ad34dc899afc87
Pallab Nandi <pallabnandi6@gmail.com>
2022-07-03 12:05:40 +0530

    2nd highest another way

---
+++ second_highest2.js
@@ -0,0 +1,13 @@
+/**
+ * 1. we sort the array in descending manner.
+ * 2. we choose index = 1 for 2nd highest.
+ */
+
+
+function secondHighest(val) {
+    val.sort((a, b) => a - b);
+    return val[val.length - 2]
+}
+
+let arr = [2,6,1,34,23,64,12,9];
+console.log(secondHighest(arr));
