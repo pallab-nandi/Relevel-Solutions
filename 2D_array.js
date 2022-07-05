27b183c57543b6d0fd5feb34120f54c751347afd
Pallab Nandi <pallabnandi6@gmail.com>
2022-07-05 20:52:09 +0530

    2D array

---
+++ 2D_array.js
@@ -0,0 +1,33 @@
+/**
+ * Q-1 - How to define 2D array?
+ * 
+ * Q - Why we have to define 2D array
+ * ANS - When we need to store data in table format then we use 2-D Array
+ * 
+ * Steps to Define 2D Array
+ * Step 1 - We will define normal Array
+ */
+
+let oneDArray = [   12, 
+                    23, 
+                    45
+                ];
+
+let twoDArray = [
+                    ['Mayank', 'B.tech', 'IIT-K', 69],
+                    ["Arrohoi", "B.tech", "NSIT", 80],
+                    ["Pawan", "B.tech", "BITS", 80]
+                ]
+
+console.log(twoDArray[0][0]);
+console.log(twoDArray[0][1]);
+console.log(twoDArray[0][2]);
+console.log(twoDArray[0][3]);
+console.log(twoDArray[1][0]);
+console.log(twoDArray[1][1]);
+console.log(twoDArray[1][2]);
+console.log(twoDArray[1][3]);
+console.log(twoDArray[2][0]);
+console.log(twoDArray[2][1]);
+console.log(twoDArray[2][2]);
+console.log(twoDArray[2][3]);
