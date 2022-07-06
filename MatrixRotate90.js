903032168527d41acc76caf627c02707c1623bd8
Pallab Nandi <pallabnandi6@gmail.com>
2022-07-06 19:50:17 +0530

    5th June - Homework2

---
+++ MatrixRotate90.js
@@ -0,0 +1,10 @@
+/**
+ * Q - WE HAVE TO ROTATE A SQUARE MATRIX IN 90 DEGREE
+ * Ans - Steps have to follow :--
+ *      STEP 1 - Declare a Square Matrix
+ *      STEP 2 - Observe what happen when we rotate it to 90 degree clockwise
+ *      STEP 3 - We observed if we rotate a square matrix into 90 degree clockwise, then it's first row 
+ * interchange the last column, then second row interchange last second column
+ *      STEP 4 - So we have to use two loops to make it interchange
+ *      STEP 5 - One loop starts first row and second loop starts with last column to interchange.
+ */
