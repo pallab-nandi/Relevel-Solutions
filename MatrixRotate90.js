13b06516db7093748f760cc8711ea4397bd242c1
Pallab Nandi <pallabnandi6@gmail.com>
2022-07-07 18:10:42 +0530

    Complete 90 deg clockwise matrix rotation program

--- MatrixRotate90.js
+++ MatrixRotate90.js
@@ -8,3 +8,34 @@
  *      STEP 4 - So we have to use two loops to make it interchange
  *      STEP 5 - One loop starts first row and second loop starts with last column to interchange.
  */
+
+function rotate90CW(arr) {
+    let result = [];
+    for(let i = 0; i < arr.length; i++) {
+        let newArr = [];
+        for(let j = arr.length - 1; j >= 0; j--) {
+            newArr.push(arr[j][i])
+        }
+        result.push(newArr)
+    }
+    return result;
+}
+
+let arr = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
+
+console.log(rotate90CW(arr));
+
+/**
+ * OUTPUT ::
+ *      [
+            [ 13, 9, 5, 1 ], 
+            [ 14, 10, 6, 2 ],
+            [ 15, 11, 7, 3 ],
+            [ 16, 12, 8, 4 ] 
+        ]
+ */
